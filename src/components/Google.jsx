b41b114a97bfa644a5cad4c73841a9637fb92582
shauryaguleria007 <shauryaguleria12345678@gmail.com>
2023-01-31 21:10:41 +0530

    a

--- src/components/Google.jsx
+++ src/components/Google.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 export const Google = ({ value }) => {
   const googleAuth = () => {
-    window.open(`http://localhost:3000/api/v1/auth/login/google`, '_self')
+    window.open(`https://localhost:3000/api/v1/auth/login/google`, '_self')
   }
   return (
     <div className='regester'>
--- src/services/userApi.js
+++ src/services/userApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const userApi = createApi({
   reducerPath: 'userApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3000/api/v1/',
+    baseUrl: 'https://localhost:3000/api/v1/',
     withCredentials: true,
     credentials: 'include',
   }),
